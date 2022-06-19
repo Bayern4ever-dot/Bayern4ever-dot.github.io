2a11f638ad2873bb6dcf8aab261e8aa131e4337f
little rookie <358176794@qq.com>
2022-06-19 09:23:52 +0800

    Site updated: 2022-06-19 09:23:51

---
+++ css/background.css
@@ -0,0 +1,3 @@
+#web_bg{
+    background-image: url("https://ae01.alicdn.com/kf/H18a4b998752a4ae68b8e85d432a5aef0l.png"), linear-gradient(60deg, rgba(255, 165, 150, 0.5) 5%, rgba(0, 228, 255, 0.35)), url("https://ae01.alicdn.com/kf/H21b5f6b8496141a1979a33666e1074d9x.jpg");
+}
